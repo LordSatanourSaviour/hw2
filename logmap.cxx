eb78c87389ec4452497d370f16239ae8d798a0f2
Mara Wiltshire <Malaria@Malaria.local>
2015-11-10 21:52:50 +0100

    here's hw 2

--- logmap.cxx
+++ logmap.cxx
@@ -1,23 +1,33 @@
+
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main(){
-	double x0 = 0.5;
-	double x;
-	int Nskip = 100; //Number of iterations to skip
-	int Nend  = 200; //Number of total iterations
-
-	for(double r=0; r <= 4; r += 0.001){
-	   x=x0;
-	   for(int i=1; i <= Nskip; i++)
-		   x = r*x*(1-x);
-	   for(int i=Nskip+1; i <= Nend; i++){
-	   		   x = r*x*(1-x);
-	   		   cout << r << "\t" << x << endl;
-   	   }
-	}
-
-
-	return 0;
+    double x0 = 0.5;
+    double x;
+    int Nskip = 100; //Number of iterations to skip
+    int Nend  = 200; //Number of total iterations
+    double xx[Nend-Nskip];
+    ofstream out("output_logmap.dat");
+    
+    for(double r=0; r <= 4; r += 0.001){
+        x=x0;
+        for(int k=1; k <= Nskip; k++)
+            x = r*x*(1-x);
+        for(int i=Nskip+1; i <= Nend; i++){
+            x = r*x*(1-x);
+            xx[i-Nskip-1]=x;
+            
+            
+        }
+        for(int j=0;j<=(Nend-Nskip);j++){
+            out << r << "\t" << xx[j] << endl;
+            
+        }
+    }
+    
+    out.close();
+    return 0;
 }
